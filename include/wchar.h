316c87a6a606adeb4415ea91c82c5638ab42c0b4
sin <sin@2f30.org>
2015-03-03 20:38:52 +0000

    Style fix

--- include/wchar.h
+++ include/wchar.h
@@ -42,13 +42,13 @@ __fortify_mbsrtowcs(wchar_t *d, const char **s,
 
 static inline __attribute__ ((always_inline))
 size_t
-__fortify_mbstowcs(wchar_t *d, const char *s, size_t wn)
+__fortify_mbstowcs(wchar_t *d, const char *s, size_t n)
 {
 	size_t bos = __builtin_object_size(d, 0);
 
-	if (wn > bos / sizeof(wchar_t))
+	if (n > bos / sizeof(wchar_t))
 		__builtin_trap();
-	return mbstowcs(d, s, wn);
+	return mbstowcs(d, s, n);
 }
 
 static inline __attribute__ ((always_inline))
@@ -91,7 +91,7 @@ __fortify_wmemset(wchar_t *s, wchar_t c, size_t n)
 #undef mbsrtowcs
 #define mbsrtowcs(d, s, wn, st) __fortify_mbsrtowcs(d, s, wn, st)
 #undef mbstowcs
-#define mbstowcs(d, s, wn) __fortify_mbstowcs(d, s, wn)
+#define mbstowcs(d, s, n) __fortify_mbstowcs(d, s, n)
 #undef wmemcpy
 #define wmemcpy(d, s, n) __fortify_wmemcpy(d, s, n)
 #undef wmemmove
