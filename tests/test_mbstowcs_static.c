bc0c8a57cb13fb110b3f9e050d67ef2d5678f85f
jvoisin <julien.voisin@dustri.org>
2023-08-21 22:49:21 +0200

    Add tests for mbstowcs

---
+++ tests/test_mbstowcs_static.c
@@ -0,0 +1,15 @@
+#include "common.h"
+
+#include <wchar.h>
+
+int main(int argc, char** argv) {
+  const char* mbstr = "z\u00df\u6c34\U0001f34c"; // or u8"zß水🍌"
+  wchar_t wstr[5];
+  mbstowcs(wstr, mbstr, 4);
+
+  CHK_FAIL_START
+  mbstowcs(wstr, mbstr, 16);
+  CHK_FAIL_END
+
+  return ret;
+}
