1403ff93afcdd3fea2d18a05b73a9dd11c81a21f
jvoisin <julien.voisin@dustri.org>
2023-09-04 01:46:59 +0200

    Add tests for wmemcpy

---
+++ tests/test_wmemcpy_dynamic_write.c
@@ -0,0 +1,14 @@
+#include "common.h"
+
+#include <wchar.h>
+
+int main(int argc, char** argv) {
+  wchar_t buffer[2] = {0};
+  wmemcpy(buffer, L"αβγδεζηθικλμνξοπρστυφχψω", 2);
+
+  CHK_FAIL_START
+  wmemcpy(buffer, L"αβγδεζηθικλμνξοπρστυφχψω", argc);
+  CHK_FAIL_END
+
+  return ret;
+}
