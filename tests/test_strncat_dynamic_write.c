1122a7b8d9ab1c6ea030fac11495fc75aae07d42
jvoisin <julien.voisin@dustri.org>
2023-09-17 15:48:42 +0200

    Add dynamic tests for strcpy and strncat

---
+++ tests/test_strncat_dynamic_write.c
@@ -0,0 +1,16 @@
+#include "common.h"
+
+#include <string.h>
+
+int main(int argc, char** argv) {
+  char buffer[8] = {0};
+  strncat(buffer, "1234567", 5);
+  puts(buffer);
+
+  CHK_FAIL_START
+  strncat(buffer, argv[1], argc);
+  CHK_FAIL_END
+
+  puts(buffer);
+  return ret;
+}
