267d5dbe4f79b8ebff8588700b2a8c0d1e56fc97
jvoisin <julien.voisin@dustri.org>
2023-09-17 16:02:08 +0200

    Add more dynamic tests

---
+++ tests/test_strlcpy_static_write.c
@@ -0,0 +1,19 @@
+#define _GNU_SOURCE
+#define _BSD_SOURCE
+
+#include "common.h"
+
+#include <string.h>
+
+int main(int argc, char** argv) {
+  char buffer[8] = {0};
+  strlcpy(buffer, "1234567", sizeof(buffer));
+  puts(buffer);
+
+  CHK_FAIL_START
+  strlcpy(buffer, "1234567890", 10);
+  CHK_FAIL_END
+
+  puts(buffer);
+  return ret;
+}
